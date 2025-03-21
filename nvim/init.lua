539956c5f0bee17d5a74201adb2bb9688f2f8167
zblednu <maksym.surovtsev@student.tuke.sk>
2025-03-21 16:58:42 +0100

    something changed

--- nvim/init.lua
+++ nvim/init.lua
@@ -690,6 +690,7 @@ require("lazy").setup({
 				-- clangd = {},
 				-- gopls = {},
 				pyright = {},
+				deno = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
@@ -697,7 +698,7 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				ts_ls = {},
+				-- ts_ls = {},
 				--
 
 				lua_ls = {
